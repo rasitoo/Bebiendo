9342391362a4d9473ab99a2d3f622073efb0744f
Rodrigo <05rotapi@gmail.com>
2025-02-11 11:41:22 +0100

    notify y wait

--- src/main/java/HiloCamarero.java
+++ src/main/java/HiloCamarero.java
@@ -3,19 +3,27 @@
  * @date 11 febrero, 2025
  */
 public class HiloCamarero extends Thread {
-    private  Vaso v;
+    private Vaso v;
 
     public HiloCamarero(Vaso vaso) {
         this.v = vaso;
     }
+
     public void run() {
-        while (v.consultarVeces() <= 3) {
-            if (!v.estaLLeno()){
-                v.retardar();
+        while (v.consultarVeces() < 3) {
+            synchronized (v) {
+                while (v.estaLLeno()) {
+                    try {
+                        System.out.println("No puedes llenar el vaso todavía");
+                        v.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
                 v.llenarVaso();
                 System.out.println("Se ha llenado el vaso");
+                v.notifyAll();
             }
-            else  {System.out.println("No se puede llenar el vaso, cabezón");}
         }
     }
 }
--- src/main/java/HiloCliente.java
+++ src/main/java/HiloCliente.java
@@ -11,13 +11,21 @@ public class HiloCliente extends Thread {
 
     public void run() {
         while (v.consultarVeces() <= 3) {
-            if (v.estaLLeno()) {
+            synchronized (v) {
+                while (!v.estaLLeno()) {
+                    try {
+                        System.out.println("No puedes beber, el vaso está vacío");
+                        v.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
                 v.retardar();
                 v.vaciarVaso();
                 v.incrementarVeces();
                 System.out.println("El cliente se ha bebido el vaso");
-            } else
-                System.out.println("El cliente no puede beber, su vaso está vacío, y eres un cabezón");
+                v.notifyAll();
+            }
         }
         System.out.println("El cliente ya ha bebido 3 veces");
     }
